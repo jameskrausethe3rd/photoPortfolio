081207d34fdf04c4a745f9c3f6558210625a2235
James Krause <jameskrausethe3rd@gmail.com>
2025-04-22 19:46:39 -0500

    Fixes

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import BioCard from './BioCard';
 
 firebase.initializeApp(firebaseConfig);
 
-const ImageGallery = () => {
+const ImageGallery = ({ test = false }) => {
   const [images, setImages] = useState([]);
   const [selectedImage, setSelectedImage] = useState(null);
   const [currentPage, setCurrentPage] = useState(1);
@@ -18,38 +18,87 @@ const ImageGallery = () => {
   const observerRef = useRef(null);
   const reversedItemsRef = useRef([]);
 
+  // Local test images
+  const localImages = [
+    { url: "/photoPortfolio/images/2025-04-12_JRK02480.jpg", name: "Test1"},
+    { url: "/photoPortfolio/images/2025-04-12_JRK02486.jpg", name: "Test2"},
+    { url: "/photoPortfolio/images/2025-04-12_JRK02489.jpg", name: "Test3"},
+    { url: "/photoPortfolio/images/2025-04-12_JRK02490.jpg", name: "Test4"},
+    { url: "/photoPortfolio/images/2025-04-12_JRK02493.jpg", name: "Test5"},
+    { url: "/photoPortfolio/images/2025-04-12_JRK02496.jpg", name: "Test6"},
+    { url: "/photoPortfolio/images/2025-04-19_JRK02504.jpg", name: "Test7"},
+    { url: "/photoPortfolio/images/2025-04-19_JRK02505.jpg", name: "Test8"},
+    { url: "/photoPortfolio/images/2025-04-19_JRK02506.jpg", name: "Test9"},
+    { url: "/photoPortfolio/images/2025-04-19_JRK02508.jpg", name: "Test10"},
+    { url: "/photoPortfolio/images/2025-04-19_JRK02509.jpg", name: "Test11"},
+    { url: "/photoPortfolio/images/2025-04-19_JRK02510.jpg", name: "Test12"},
+    { url: "/photoPortfolio/images/2025-04-19_JRK02512.jpg", name: "Test13"},
+    { url: "/photoPortfolio/images/2025-04-19_JRK02513.jpg", name: "Test14"},
+    { url: "/photoPortfolio/images/2025-04-19_JRK02515.jpg", name: "Test15"},
+    { url: "/photoPortfolio/images/2025-04-19_JRK02519.jpg", name: "Test16"},
+    { url: "/photoPortfolio/images/2025-04-19_JRK02521.jpg", name: "Test17"},
+    { url: "/photoPortfolio/images/2025-04-19_JRK02522.jpg", name: "Test18"},
+    { url: "/photoPortfolio/images/2025-04-19_JRK02523.jpg", name: "Test19"},
+    { url: "/photoPortfolio/images/2025-04-19_JRK02526.jpg", name: "Test20"},
+    { url: "/photoPortfolio/images/2025-04-19_JRK02527.jpg", name: "Test21"},
+    { url: "/photoPortfolio/images/2025-04-19_JRK02531.jpg", name: "Test22"},
+    { url: "/photoPortfolio/images/2025-04-19_JRK02533.jpg", name: "Test23"},
+    { url: "/photoPortfolio/images/2025-04-19_JRK02536.jpg", name: "Test24"},
+    { url: "/photoPortfolio/images/2025-04-19_JRK02537.jpg", name: "Test25"},
+    { url: "/photoPortfolio/images/2025-04-19_JRK02539.jpg", name: "Test26"},
+  ];
+
   const getNewSetOfUrls = useCallback(async () => {
-    const startAt = (currentPage - 1) * imagesPerPage;
-    const endAt = startAt + imagesPerPage;
-    const reversedItems = reversedItemsRef.current;
-  
-    const urls = await Promise.all(
-      reversedItems.slice(startAt, endAt).map(async (item) => {
-        const url = await item.getDownloadURL();
-        return { url, name: item.name };
-      })
-    );
-  
-    setImages((prevImages) => {
-      const filteredUrls = urls.filter(
-        (newImage) => !prevImages.some((image) => image.url === newImage.url)
+    if (test) {
+      // Use local images for testing
+      const startAt = (currentPage - 1) * imagesPerPage;
+      const endAt = startAt + imagesPerPage;
+      localImages.reverse();
+      const urls = localImages.slice(startAt, endAt);
+
+      if (startAt && endAt < localImages.length) {
+        setImages((prevImages) => [...prevImages, ...urls]);
+      }
+    } else {
+      // Fetch images from Firebase
+      const startAt = (currentPage - 1) * imagesPerPage;
+      const endAt = startAt + imagesPerPage;
+      const reversedItems = reversedItemsRef.current;
+
+      const urls = await Promise.all(
+        reversedItems.slice(startAt, endAt).map(async (item) => {
+          const url = await item.getDownloadURL();
+          return { url, name: item.name };
+        })
       );
-      return [...prevImages, ...filteredUrls];
-    });
-  }, [currentPage, imagesPerPage]);
+
+      setImages((prevImages) => {
+        const filteredUrls = urls.filter(
+          (newImage) => !prevImages.some((image) => image.url === newImage.url)
+        );
+        return [...prevImages, ...filteredUrls];
+      });
+    }
+  }, [currentPage, imagesPerPage, test]);
   
   useEffect(() => {
-    const fetchInitialImages = async () => {
-      const storageRef = firebase.storage().ref();
-      const imagesRef = storageRef.child('images');
-      const imageList = await imagesRef.listAll();
-  
-      reversedItemsRef.current = imageList.items.reverse(); // Save once
-      await getNewSetOfUrls(); // Load page 1
-    };
-  
-    fetchInitialImages();
-  }, []);
+    if (test) {
+      // Load local images for testing
+      getNewSetOfUrls();
+    } else {
+      // Fetch images from Firebase
+      const fetchInitialImages = async () => {
+        const storageRef = firebase.storage().ref();
+        const imagesRef = storageRef.child('images');
+        const imageList = await imagesRef.listAll();
+
+        reversedItemsRef.current = imageList.items.reverse(); // Save once
+        await getNewSetOfUrls(); // Load page 1
+      };
+
+      fetchInitialImages();
+    }
+  }, [test, getNewSetOfUrls]);
 
   useEffect(() => {
     const observer = new IntersectionObserver(
@@ -60,14 +109,14 @@ const ImageGallery = () => {
       },
       {
         threshold: 0.5,
-        rootMargin: "500px",
+        rootMargin: '500px',
       }
     );
-  
+
     if (observerRef.current) {
       observer.observe(observerRef.current);
     }
-  
+
     return () => {
       if (observerRef.current) {
         observer.unobserve(observerRef.current);
@@ -95,7 +144,7 @@ const ImageGallery = () => {
         <div className="grid-container">
           <div className="row">
             {images.map((image, index) => (
-              <ImageCard key={image.url} image={image} openModal={openModal} />
+              <ImageCard key={`${image.url}-${index}`} image={image} openModal={openModal} />
             ))}
           </div>
           <div ref={observerRef} style={{ height: '1px' }}></div>
